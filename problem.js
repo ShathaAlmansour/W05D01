ada12f0b5fffd62f4679fa53ec3a6d49b4275297
ShathaAlmansour <>
2021-11-08 00:03:06 +0300

    added

--- problem.js
+++ problem.js
@@ -1,4 +1,19 @@
-function isPalindrome(x) {
+  function Palindrome(str) {
+    const arr = string.split('');
+    const arr = arr.reverse();
+    const reverseString = reverseArr.join('');
+    if(string == reverseString) {
+        console.log('It is a palindrome');
+    }
+    else {
+        console.log('It is not a palindrome');
+    }
+}
+const string = prompt('Enter a string: ');
+Palindrome(string);
+
+
+// function isPalindrome(x) {
      
-    return x.toLowerCase().split('').reverse().join('') === x.toLowerCase();
-  }
+//     return x.toLowerCase().split('').reverse().join('') === x.toLowerCase();
+//   }
