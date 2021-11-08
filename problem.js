0bb131dcef69c80279950f600f3f4d6f15d121a9
ShathaAlmansour <>
2021-11-08 09:21:55 +0300

    added

--- problem.js
+++ problem.js
@@ -13,13 +13,19 @@ const string = prompt('Enter a string: ');
 Palindrome(string);
 
 
-// /Teacher
-
-// // function isPalindrome(x) {
-     
-// //    const x.toLowerCase().split("").reverse().join("") === x.toLowerCase();
+// Teacher
+//  function isPalindrome(x) {    
+//    const x.toLowerCase().split("").reverse().join("") === x.toLowerCase();
 // return x.toLowerCase () === reverse
-// //   }
+//   }
 
-
-
+// Teacher 
+// function isPalindrome (x) {
+//     const str = x.toLowerCase () 
+//     let length = str.length - 1 
+//     for (let i = 0 ; i < str.length ; i++ ) {
+//         if (str [i] !== str [length-i]){
+//         }
+//     }
+// return true 
+// }
